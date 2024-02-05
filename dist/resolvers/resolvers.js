40fe3d72374639111e67a26a1a4c935750cb36d8
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-02-05 19:07:53 +0100

    test exemple with users in graphQL

--- dist/resolvers/resolvers.js
+++ dist/resolvers/resolvers.js
@@ -1,8 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        age: 25,
+    },
+    { id: 2, name: 'Jane Doe', email: 'jane@gmail.com', age: 26 },
+];
 const resolvers = {
     Query: {
         hello: () => 'Hello, World!',
-    },
+        users: () => users,
+        user: (parent, args) => users.find((user) => user.id === Number(args.id))
+    }, // Add a comma here
 };
 exports.default = resolvers;
--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,7 +1,20 @@
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    age: 25,
+  },
+  { id: 2, name: 'Jane Doe', email: 'jane@gmail.com', age: 26 },
+];
+
+
 const resolvers = {
   Query: {
     hello: () => 'Hello, World!',
-  },
+    users: () => users,
+    user: (parent, args: {id: number | string}) => users.find((user) => user.id === Number(args.id))
+  }, // Add a comma here
 };
 
 export default resolvers;
