60d44144f659da8db3f40ed14192a2c6459bd722
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-02-06 14:15:46 +0100

    Set the cache option to bounded in your ApolloServer constructor

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,14 @@ const apollo_server_1 = require("apollo-server");
 const resolvers_1 = __importDefault(require("./resolvers/resolvers"));
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
-const pathName = "../src/graphql/schema.graphql";
+const pathName = '../src/graphql/schema.graphql';
 const schemaPath = path_1.default.join(__dirname, pathName);
 const typeDefs = fs_1.default.readFileSync(schemaPath, 'utf-8');
-const server = new apollo_server_1.ApolloServer({ typeDefs, resolvers: resolvers_1.default });
+const server = new apollo_server_1.ApolloServer({
+    typeDefs,
+    resolvers: resolvers_1.default,
+    cache: 'bounded',
+});
 const port = process.env.PORT || 4000;
 server.listen({ port }).then(({ url }) => {
     console.log(`Server ready at ${url}`);
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import resolvers from './resolvers/resolvers';
 import path from 'path';
 import fs from 'fs';
 
-const pathName ="../src/graphql/schema.graphql";
+const pathName = '../src/graphql/schema.graphql';
 const schemaPath = path.join(__dirname, pathName);
 const typeDefs = fs.readFileSync(schemaPath, 'utf-8');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cache: 'bounded',
+});
 
 const port = process.env.PORT || 4000;
 
-server.listen({port}).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
